52acb317fc72af2b242b9926ac21cee416bc3dc9
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-09-16 14:13:06 +0100

    Ex 02: add comparison with list behaviour

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tjorge-l < tjorge-l@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 13:44:20 by tjorge-l          #+#    #+#             */
-/*   Updated: 2025/09/16 13:44:20 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2025/09/16 14:12:43 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,3 +43,34 @@ int	main(void)
 	std::stack<int> s(mstack);
 	return (0);
 }
+
+// int	main(void)
+// {
+// 	std::list<int>	mstack;
+
+// 	mstack.push_back(5);
+// 	mstack.push_back(17);
+
+// 	std::cout << mstack.back() << std::endl;
+
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push_back(3);
+// 	mstack.push_back(5);
+// 	mstack.push_back(737);
+// 	mstack.push_back(0);
+
+// 	std::list<int>::iterator it = mstack.begin();
+// 	std::list<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	std::deque<int> d(mstack.begin(), mstack.end());
+// 	std::stack<int> s(d);
+// 	return (0);
+// }

da7cad0ae5013b72b1dc9257c372156df43f30f0
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-10-18 14:44:30 +0100

    Add tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,13 @@
 /*   By: tjorge-l < tjorge-l@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 17:50:55 by tjorge-l          #+#    #+#             */
-/*   Updated: 2025/09/15 17:50:55 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2025/10/18 14:42:31 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <vector>
+#include <deque>
 #include <iostream>
 #include <list>
 
@@ -20,17 +21,20 @@ int	main(void)
 	std::vector<int> v(3, 3);
 	std::cout << easyfind(v, 3) << std::endl;
 
-	// std::list<int> l(2, 3);
-	// std::cout << easyfind(l, 3) << std::endl;
+	std::list<int> l(2, 3);
+	std::cout << easyfind(l, 3) << std::endl;
 
-	// try
-	// {
-	// 	easyfind(v, 4);
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
+	std::deque<int> d(2, 3);
+	std::cout << easyfind(d, 3) << std::endl;
+
+	try
+	{
+		easyfind(v, 4);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	
 	return (0);
 }
